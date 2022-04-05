bfdb8abef1fc31e3e11d18e96be00052e121f67c
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-04-05 16:32:57 +0530

    Add files via upload

---
+++ sender.cpp
@@ -0,0 +1,26 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include"test/catch.hpp"
+#include"sender.h"
+
+int stubSensorSimulation()
+{  
+   static int value;
+   value++;
+   return value;
+}
+
+
+TEST_CASE("Checks the sender output Temperature and SOC physical value which is printed in a structure")
+ {
+	batteryParameter* expectedOutput = {{-39,1} ,{-38,1},{-37,2},{-36,2},{-35,2},{-34,2},{-33,2},{-32,2} };
+	batteryParameter* actualOutput = getFromSensorSendToConsole(8,&stubSensorSimulation,&stubSensorSimulation,q1p0_o40,q0p5_o0,&printOnConsole);
+	for (int i=1;i<5;i++)
+	{
+		REQUIRE(expectedOutput[i].temp = actualOutput[i].temp);
+		REQUIRE(expectedOutput[i].soc = actualOutput[i].soc);
+	}
+	free(actualOutput);
+	free(expectedOutput);
+	
+}
