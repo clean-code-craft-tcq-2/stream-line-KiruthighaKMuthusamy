c1419c01617482db2b460d2e7c252715941d1a25
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-04-05 19:06:26 +0530

    Update sender.cpp

--- sender.cpp
+++ sender.cpp
@@ -13,8 +13,8 @@ int stubSensorSimulation()
 
 TEST_CASE("Checks the sender output Temperature and SOC physical value which is printed in a structure")
  {
-	batteryParameter expectedOutput[8] = {{-39,5} ,{-38,5},{-37,6},{-36,6},{-35,7},{-34,7},{-33,8},{-32,8} };
-	batteryParameter* actualOutput = getFromSensorSendToConsole(8,&stubSensorSimulation,&stubSensorSimulation,q1p0_o40,q0p5_o0,&printOnConsole);
+	batteryParameter expectedOutput[4] = {{-39,5} ,{-38,5},{-37,6},{-36,6}};
+	batteryParameter* actualOutput = getFromSensorSendToConsole(4,&stubSensorSimulation,&stubSensorSimulation,q1p0_o40,q0p5_o0,&printOnConsole);
 	for (int i=1;i<5;i++)
 	{
 		REQUIRE(expectedOutput[i].temp == actualOutput[i].temp);
