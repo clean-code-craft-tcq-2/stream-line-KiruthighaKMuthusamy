350864b658bf574d00fc99314b6ce42aab1a69a0
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-18 21:39:20 +0530

    Update receiver.c

--- RECEIVER/receiver.c
+++ RECEIVER/receiver.c
@@ -1,1 +1,7 @@
+#include <stdio.h>
+#include "receiver.h"
 
+void getStreamOfValues(batteryParameter* tempSocValuesInBAtt, int noOfValues) /* tempSocValues are available from the printconsole from the sender*/
+{
+  
+}
