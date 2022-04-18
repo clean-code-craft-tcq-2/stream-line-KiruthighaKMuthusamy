f204acf1c02192a9e9b0944c15a14b330baa4b9b
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-18 21:38:37 +0530

    Update receiver.h

--- RECEIVER/receiver.h
+++ RECEIVER/receiver.h
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+struct batteryParameter
+{
+  float temperature;
+  float SOC;
+};
+
+void getStreamOfValues(batteryParameter* tempSocValuesInBAtt, int noOfValues);
+void getMinimumValue(batteryParameter* tempSocValuesInBAtt, int noOfValues);
+void getMaximumValue(batteryParameter* tempSocValuesInBAtt, int noOfValues);
