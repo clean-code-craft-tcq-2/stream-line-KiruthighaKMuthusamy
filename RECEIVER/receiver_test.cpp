a149622598b910f998af7c972c9b834d88821e13
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-20 19:14:52 +0530

    Update receiver_test.cpp

--- RECEIVER/receiver_test.cpp
+++ RECEIVER/receiver_test.cpp
@@ -6,7 +6,7 @@ TEST_CASE("Check for minimum temperature value")
 {
   float result;
   batteryParameter* tempSocValuesInBAtt;
-  tempSocValuesInBAtt->temperature[3] = {2.3,3.9,68.2};
+  tempSocValuesInBAtt.temperature[3] = {2.3,3.9,68.2};
   int noOfValues = (sizeof(tempSocValuesInBAtt->temperature[3]))/(sizeof(int));
   //tempSocValuesInBAtt->SOC[3] = {36,2.6,2.3};
   float expectedTemp = 2.3;
