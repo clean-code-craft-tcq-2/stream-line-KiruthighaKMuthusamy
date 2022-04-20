426a4d43dc6514a62e0659eedae3faafea471c86
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-20 17:24:14 +0530

    Update receiver_test.cpp

--- RECEIVER/receiver_test.cpp
+++ RECEIVER/receiver_test.cpp
@@ -5,9 +5,9 @@
 TEST_CASE("Check for minimum temperature value")
 {
   float result;
-  batteryParameter* tempSocValuesInBAtt.temperature[3] = {{23,25.5,99.9};
-  batteryParameter* tempSocValuesInBAtt.SOC[3] = {{36,2.6,2.3};
-  float expectedTemp = 2;
+  tempSocValuesInBAtt->temperature[3] = {23,25.5,99.9};
+  tempSocValuesInBAtt->SOC[3] = {36,2.6,2.3};
+  float expectedTemp = 23;
   result = getMinimumTempValue(tempSocValuesInBAtt);
   REQUIRE(result==expectedTemp);
 }
