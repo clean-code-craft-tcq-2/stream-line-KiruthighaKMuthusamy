b9923829093b3b5c96c9abf1faddc6cd05f176ed
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-20 21:17:47 +0530

    Update receiver_test.cpp

--- RECEIVER/receiver_test.cpp
+++ RECEIVER/receiver_test.cpp
@@ -14,3 +14,30 @@ TEST_CASE("Check for minimum temperature value")
   REQUIRE(result==expectedTemp);
 }
 
+TEST_CASE("Check for minimum SOC value")
+{
+  float result;
+  float SOC[] = {36.0, 31.1, 15, 89};
+  float expectedSOC = 15;
+  int noOfValues = (sizeof(SOC))/(sizeof(int));
+  result = getMinimumSOCValue(SOC,noOfValues);
+  REQUIRE(result==expectedSOC);
+}
+TEST_CASE("Check for maximum temperature value")
+{
+  float result;
+  float temp[] = {1.2, 1.1, 15.6, 89.2};
+  float expectedTemp = 89.2;
+  int noOfValues = (sizeof(temp))/(sizeof(int));
+  result = getMaximumTempValue(temp,noOfValues);
+  REQUIRE(result==expectedTemp);
+}
+TEST_CASE("Check for maximum SOC value")
+{
+  float result;
+  float SOC[] = {36.0, 31.1, 15, 89};
+  float expectedSOC = 89;
+  int noOfValues = (sizeof(SOC))/(sizeof(int));
+  result = getMaximumSOCValue(SOC,noOfValues);
+  REQUIRE(result==expectedSOC);
+}
