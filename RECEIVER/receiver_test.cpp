a09d1216b2af82b9b9bb7f0d73804b4f740944a1
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-20 20:08:34 +0530

    Update receiver_test.cpp

--- RECEIVER/receiver_test.cpp
+++ RECEIVER/receiver_test.cpp
@@ -6,6 +6,9 @@
 TEST_CASE("Check for minimum temperature value")
 {
   float result;
+  float temp[] = {1.2, 1.1, 15.6, 89.2};
+  float expectedTemp = 1.1;
+  int noOfValues = (sizeof(temp[]))/(sizeof(int));
   result = getMinimumTempValue(tempSocValuesInBAtt,noOfValues);
   REQUIRE(result==expectedTemp);
 }
