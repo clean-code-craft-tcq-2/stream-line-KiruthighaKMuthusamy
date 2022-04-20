6e5fe7ab23de3d90acc20985fe824dae182b9f31
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-20 17:02:48 +0530

    Update receiver.h

--- RECEIVER/receiver.h
+++ RECEIVER/receiver.h
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-struct batteryParameter
-{
-  float temperature;
-  float SOC;
-};
-
 /*Assuming no negative values in the stream*/
 #define MIN_TEMP_VALUE 0
 #define MAX_TEMP_VALUE 100
 #define MIN_SOC_VALUE 0
 #define MAX_SOC_VALUE 100
+#define noOfValues 50
 
-extern float getMinimumTempValue(batteryParameter* tempSocValuesInBAtt, int noOfValues);
-extern float getMinimumSOCValue(batteryParameter* tempSocValuesInBAtt, int noOfValues);
-extern float getMaximumTempValue(batteryParameter* tempSocValuesInBAtt, int noOfValues);
-extern float getMaximumSOCValue(batteryParameter* tempSocValuesInBAtt, int noOfValues);
+struct batteryParameter
+{
+  float temperature[noOfValues];
+  float SOC[noOfValues];
+};
+
+extern float getMinimumTempValue(batteryParameter* tempSocValuesInBAtt);
+extern float getMinimumSOCValue(batteryParameter* tempSocValuesInBAtt);
+extern float getMaximumTempValue(batteryParameter* tempSocValuesInBAtt);
+extern float getMaximumSOCValue(batteryParameter* tempSocValuesInBAtt);
 
