92be28dcd9a7f144967635c31166cf886399e0e2
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-20 20:12:30 +0530

    Update receiver.h

--- RECEIVER/receiver.h
+++ RECEIVER/receiver.h
@@ -9,8 +9,8 @@
 
 struct 
 {
-  float temperature[3];
-  float SOC[3];
+  float temperature[NO_OF_VALUES];
+  float SOC[NO_OF_VALUES];
 }batteryParameter;
 
 extern float getMinimumTempValue(batteryParameter* tempSocValuesInBAtt, int noOfValues);
