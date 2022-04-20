81cab7d16d25bbc83b19805f6a7ea5e1284a2708
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-20 14:43:04 +0530

    Update receiver.c

--- RECEIVER/receiver.c
+++ RECEIVER/receiver.c
@@ -1,7 +1,58 @@
 #include <stdio.h>
 #include "receiver.h"
 
-void getStreamOfValues(batteryParameter* tempSocValuesInBAtt, int noOfValues) /* tempSocValues are available from the printconsole from the sender*/
+float getMinimumTempValue(batteryParameter* tempSocValuesInBAtt, int noOfValues) /* tempSocValuesInBAtt are available from the printconsole from the sender*/
 {
-  
+  int i;
+  float temp_min = MAX_TEMP_VALUE;
+  for(i=0; i<noOfValues; i++)
+  {
+    if(tempSocValuesInBAtt[i].temperature < temp_min)
+    {
+      temp_min = tempSocValuesInBAtt[i].temperature;
+    }    
+  }
+  return temp_min;
+}
+
+float getMinimumSOCValue(batteryParameter* tempSocValuesInBAtt, int noOfValues) /* tempSocValuesInBAtt are available from the printconsole from the sender*/
+{
+  int i;
+  float SOC_min = MAX_SOC_VALUE;
+  for(i=0; i<noOfValues; i++)
+  {
+    if(tempSocValuesInBAtt[i].SOC < SOC_min)
+    {
+      temp_min = tempSocValuesInBAtt[i].SOC;
+    }    
+  }
+  return SOC_min;
+}
+
+float getMaximumTempValue(batteryParameter* tempSocValuesInBAtt, int noOfValues) /* tempSocValuesInBAtt are available from the printconsole from the sender*/
+{
+  int i;
+  float temp_max = MIN_TEMP_VALUE;
+  for(i=0; i<noOfValues; i++)
+  {
+    if(tempSocValuesInBAtt[i].temperature > temp_max)
+    {
+      temp_max = tempSocValuesInBAtt[i].temperature;
+    }    
+  }
+  return temp_max;
+}
+
+float getMaximumSOCValue(batteryParameter* tempSocValuesInBAtt, int noOfValues) /* tempSocValuesInBAtt are available from the printconsole from the sender*/
+{
+  int i;
+  float SOC_max = MIN_SOC_VALUE;
+  for(i=0; i<noOfValues; i++)
+  {
+    if(tempSocValuesInBAtt[i].SOC > SOC_max)
+    {
+      SOC_max = tempSocValuesInBAtt[i].SOC;
+    }    
+  }
+  return SOC_max;
 }
